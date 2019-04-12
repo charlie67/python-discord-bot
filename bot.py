ffe4b6b3920e79932776dafa1630f7f3af7fa481
charlie <charlierobinson080@gmail.com>
2019-04-12 19:03:11 +0100

    Join an audio channel

--- bot.py
+++ bot.py
@@ -1,11 +1,13 @@
 # Work with Python 3.6
 import discord
 
+
 TOKEN = 'NTY2MzA5OTQ3NjUxMTI5MzQ0.XLDIpA.tUPvZxsyicFsenycpmmXkeWonDI'
 
 client = discord.Client()
 
 COMMAND_START = "-"
+
 
 @client.event
 async def on_message(message):
@@ -16,6 +18,15 @@ async def on_message(message):
     if message.content.startswith(COMMAND_START + 'hello'):
         msg = 'Hello {0.author.mention}'.format(message)
         await client.send_message(message.channel, msg)
+
+    if message.content.startswith(COMMAND_START + 'join'):
+        voice_channel_to_join = message.author.voice_channel
+
+        if voice_channel_to_join is None:
+            msg = "no channel to join dum dum"
+            await client.send_message(message.channel, msg)
+        else:
+            await client.join_voice_channel(voice_channel_to_join)
 
 
 @client.event
