f4c5d3a1f349370c2a0a44c6e681df20eb264c53
charlie <charlierobinson080@gmail.com>
2019-04-12 19:41:39 +0100

    Use the rewrite version of the discord.py library

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # discord-bot
+
+To set up: <br />
+https://discordpy.readthedocs.io/en/rewrite/intro.html
--- bot.py
+++ bot.py
@@ -1,39 +1,51 @@
 # Work with Python 3.6
 import discord
+from discord.ext import commands
 
+
+COMMAND_START = '-'
+bot = commands.Bot(command_prefix=COMMAND_START)
+bot.command()
 
 TOKEN = 'NTY2MzA5OTQ3NjUxMTI5MzQ0.XLDIpA.tUPvZxsyicFsenycpmmXkeWonDI'
 
-client = discord.Client()
 
-COMMAND_START = "-"
-
-
-@client.event
+@bot.event
 async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
+    author: discord.User = message.author
+    if author != bot.user:
+        await bot.process_commands(message)
 
-    if message.content.startswith(COMMAND_START + 'hello'):
-        msg = 'Hello {0.author.mention}'.format(message)
-        await client.send_message(message.channel, msg)
+# @client.event
+# async def on_message(message):
+#     # we do not want the bot to reply to itself
+#     if message.author == client.user:
+#         return
+#
+#     if message.content.startswith(COMMAND_START + 'hello'):
+#         msg = 'Hello {0.author.mention}'.format(message)
+#         await client.send_message(message.channel, msg)
+#
+#     if message.content.startswith(COMMAND_START + 'join'):
+#         voice_channel_to_join = message.author.voice_channel
+#
+#         if voice_channel_to_join is None:
+#             msg = "no channel to join dum dum"
+#             await client.send_message(message.channel, msg)
+#         else:
+#             await client.join_voice_channel(voice_channel_to_join)
 
-    if message.content.startswith(COMMAND_START + 'join'):
-        voice_channel_to_join = message.author.voice_channel
 
-        if voice_channel_to_join is None:
-            msg = "no channel to join dum dum"
-            await client.send_message(message.channel, msg)
-        else:
-            await client.join_voice_channel(voice_channel_to_join)
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Hey there")
 
 
-@client.event
+@bot.event
 async def on_ready():
     print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
+    print(bot.user.name)
+    print(bot.user.id)
     print('------')
 
-client.run(TOKEN)
+bot.run(TOKEN)
