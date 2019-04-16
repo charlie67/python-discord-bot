24bd270a7c35688ab47c247eeb40ea31642f5b40
Charlie Robinson <charlie@charlie.to>
2019-04-16 20:43:13 +0100

    Push up to docker hub

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
-docker build . --tag discord-bot:$TRAVIS_BRANCH-latest
+docker build . --tag charlie67/discord-bot:$TRAVIS_BRANCH-latest
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker push charlie67/discord-bot:$TRAVIS_BRANCH-latest
