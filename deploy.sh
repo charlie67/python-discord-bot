1c620c111214c6e1a33b78a13d8c7e4d2a5e1841
Charlie Robinson <charlie@charlie.to>
2019-05-09 18:49:27 +0100

    Fix travis script error

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,4 @@ then
     sudo apt-get install sshpass
     sshpass -p SSH_PASSWORD ssh SSH_URL cd /opt/discord-bot && docker-compose down && docker rmi $(docker images -a -q) && docker-compose up -d
 fi
+# comment
