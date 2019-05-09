85ee95815ced4d7719d4b1d8245296861581adc9
Charlie Robinson <charlie@charlie.to>
2019-05-09 19:21:12 +0100

    Fix deploy script error

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,8 @@ if [ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" ];
 then
     sudo apt-get update
     sudo apt-get install sshpass
-    sshpass -p $SSH_PASSWORD ssh $SSH_URL cd /opt/discord-bot && docker-compose down && docker rmi $(docker images -a -q) && docker-compose up -d
+    sshpass -p $SSH_PASSWORD ssh $SSH_URL
+    cd /opt/discord-bot
+    docker-compose down && docker rmi $(docker images -a -q) && docker-compose up -d
+    exit
 fi
