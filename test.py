3ecd2bc5c318b01864880336f91c95715778902c
Charlie Robinson <charlie@charlie.to>
2019-07-19 01:53:01 +0100

    add unit tests

---
+++ test.py
@@ -0,0 +1,28 @@
+import unittest
+import bot.voice.ytdl_impl as YTDLSource
+from bot.voice.voice_helpers import Video, PlayTypes
+
+
+class TestReceive(unittest.TestCase):
+
+    def test_get_video_search(self):
+        video: Video = YTDLSource.YTDLSource.get_video("test", "test author")
+        self.assertEqual(video.author_name, "test author")
+        self.assertEqual(video.file, False)
+        self.assertEqual(video.youtube, True)
+        self.assertEqual(video.filename, None)
+        self.assertEqual(video.play_type.value, PlayTypes.QUEUED.value)
+        self.assertTrue(video.video_url.startswith("https://www.youtube.com/watch?"))
+
+    def test_get_video_url(self):
+        video: Video = YTDLSource.YTDLSource.get_video("https://www.youtube.com/watch?v=Dqp0sMWTwwI", "test author")
+        self.assertEqual(video.author_name, "test author")
+        self.assertEqual(video.file, False)
+        self.assertEqual(video.youtube, True)
+        self.assertEqual(video.filename, None)
+        self.assertEqual(video.video_length, 60)
+        self.assertEqual(video.filename, None)
+        self.assertEqual(video.video_id, 'Dqp0sMWTwwI')
+        self.assertEqual(video.play_type.value, PlayTypes.QUEUED.value)
+        self.assertEqual(video.video_url, "https://www.youtube.com/watch?v=Dqp0sMWTwwI")
+
